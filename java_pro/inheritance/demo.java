0b2e492fcc6f4771a54696c1fe53e5f34b2f42db
Krunalsangani13 <khsangani3344@gmail.com>
2024-05-28 09:50:46 +0530

    Update demo.java

--- java_pro/inheritance/demo.java
+++ java_pro/inheritance/demo.java
@@ -5,8 +5,8 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Rectangle rectangle=new Rectangle(4, 5);
-		rectangle.area();
-		rectangle.perimeter();
+		rectangle.printArea();
+		rectangle.printPerimeter();
 		
 		Square sq=new Square(4);
 		sq.printArea();
